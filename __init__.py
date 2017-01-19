7a3a89e559c62af95f9051d386644457a2558c67
Bernat Brunet Torruella <bernat@nan-tic.com>
2017-01-19 16:58:17 +0100

    prepare code for new versions

--- __init__.py
+++ __init__.py
@@ -1,17 +1,16 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .configuration import *
-from .sale import *
+from . import configuration, sale
 
 
 def register():
     Pool.register(
-        Configuration,
-        ConfigurationCompany,
-        StockMove,
-        Sale,
-        SaleLine,
-        Move,
-        MoveLine,
+        configuration.Configuration,
+        configuration.ConfigurationCompany,
+        sale.StockMove,
+        sale.Sale,
+        sale.SaleLine,
+        sale.Move,
+        sale.MoveLine,
         module='sale_stock_account_move', type_='model')
--- sale.py
+++ sale.py
@@ -8,12 +8,12 @@ from trytond.pyson import Eval
 from trytond.transaction import Transaction
 
 __all__ = ['StockMove', 'Sale', 'SaleLine', 'Move', 'MoveLine']
-__metaclass__ = PoolMeta
 _ZERO = Decimal('0.0')
 
 
 # TODO: put it in account_invoice_stock
 class StockMove:
+    __metaclass__ = PoolMeta
     __name__ = 'stock.move'
 
     @property
@@ -35,6 +35,7 @@ class StockMove:
 
 
 class Move:
+    __metaclass__ = PoolMeta
     __name__ = 'account.move'
 
     @classmethod
@@ -46,11 +47,13 @@ class Move:
 
 
 class MoveLine:
+    __metaclass__ = PoolMeta
     __name__ = 'account.move.line'
     sale_line = fields.Many2One('sale.line', 'Sale Line')
 
 
 class Sale:
+    __metaclass__ = PoolMeta
     __name__ = 'sale.sale'
 
     @classmethod
@@ -149,6 +152,7 @@ class Sale:
 
 
 class SaleLine:
+    __metaclass__ = PoolMeta
     __name__ = 'sale.line'
 
     analytic_required = fields.Function(fields.Boolean("Require Analytics"),
