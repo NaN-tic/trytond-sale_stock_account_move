533e39f7e8c97dfaf7d761a4a135ab98fc803615
Raimon Esteve <raimon@nan-tic.com>
2018-01-10 14:33:55 +0100

    FIX run scenario tests

--- tests/test_sale_stock_account_move.py
+++ tests/test_sale_stock_account_move.py
@@ -5,7 +5,8 @@ import unittest
 import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import (doctest_setup, doctest_teardown,
+    doctest_checker)
 
 
 class TestCase(ModuleTestCase):
@@ -16,10 +17,8 @@ class TestCase(ModuleTestCase):
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_sale.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite('scenario_sale_analytic.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
